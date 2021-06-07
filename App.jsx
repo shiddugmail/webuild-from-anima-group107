ac5033d8824746e361d0e8ce81936f1de6026f68
Shiddu <30692292+shiddugmail@users.noreply.github.com>
2021-06-07 08:55:20 +0530

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,72 @@
+import "./App.css";
+import React from "react";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Group107 from "./components/Group107";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:path(|141_3992)">
+          <Group107 {...group107Data} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+const group107Data = {
+    overlapGroup1: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector@2x.png",
+    vector2: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-2@2x.png",
+    vector3: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-3@2x.png",
+    vector4: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-4@2x.png",
+    vector5: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-5@2x.png",
+    vector6: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-7@2x.png",
+    vector7: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-9@2x.png",
+    vector8: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-11@2x.png",
+    vector9: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-6@2x.png",
+    vector10: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-8@2x.png",
+    vector11: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-10@2x.png",
+    vector12: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-12@2x.png",
+    vector13: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-13@2x.png",
+    vector14: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-16@2x.png",
+    vector15: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-19@2x.png",
+    vector16: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-22@2x.png",
+    group: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-14@2x.png",
+    group2: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-17@2x.png",
+    group3: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-20@2x.png",
+    vector17: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-23@2x.png",
+    group4: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-15@2x.png",
+    group5: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-18@2x.png",
+    group6: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-21@2x.png",
+    vector18: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-24@2x.png",
+    text61: "The Webuildd Difference",
+    spanText: "What makes us stand miles apart from other firms? We call it “Webuildd Difference”: from the very first time you meet us, to the interaction with our team, the hassle-free engagement and finally the day we hand you the keys to your new home/building, the only standard we accept for ourselves (and our customers) is “Consistent Excellence”. We are never satisfied with our laurels, and our service to you goes far beyond simply putting one brick on top of another. We not only ensure that get quality, but also blend it with creativity, engineering, management, quality and multiple other disciplines to deliver the perfect output for your residential or commercial needs. We have even created our very own",
+    spanText2: " ",
+    spanText3: "cost estimator",
+    spanText4: " ",
+    spanText5: "that helps you calculate your probable capital outgo for your upcoming building. We really like to make things easy for our clients",
+    rectangle49: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/rectangle-49@1x.png",
+    group7: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-34@2x.png",
+    overlapGroup12: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-35@2x.png",
+    vector19: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-38@2x.png",
+    vector20: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-36@2x.png",
+    vector21: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-37@2x.png",
+    text65: "Our Customer Value Propositions",
+    text66: "When it comes to building a new residential or commercial building, choosing the right partner to bring your vision to life is an important first step. Our experienced Webuildd team has all the expertise, resources, and connections you need to turn inspiration into reality with seamless ease. You don’t have to deal with the hassles of managing complex coordination and multiple resources. Our in-house team has you covered from legal, estimation, site planning, architectural design,",
+    rectangle48: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/rectangle-48@1x.png",
+    vector22: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-25@2x.png",
+    vector23: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-26@2x.png",
+    vector24: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-27@2x.png",
+    vector25: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-28@2x.png",
+    vector26: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-29@2x.png",
+    vector27: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-30@2x.png",
+    vector28: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-31@2x.png",
+    vector29: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-32@2x.png",
+    vector30: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-33@2x.png",
+    text63: <>Why Choose Us? Minimum Hassles. <br />Maximum Value</>,
+    text64: "We possess third generation business continuity which is a rarity. Which means that you gain decades of collaborative experience to your benefit. Expertise, quality, transparency, reliability and cost effectiveness are why our customers come back to us again and again. We offer a perfect blend of all these factors resulting in deep customer delight. We put the “Smart” in everything that we do. Smarter design, smarter spaces, smarter value. Simply: smarter living. With exciting options and construction plans to choose from, one is certain to be right for you. Yes, other people do some of what we do, but no one does everything in the unique way we do.",
+    rectangle50: "https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/rectangle-50@1x.png",
+};
+
---
+++ components/Group107/index.jsx
@@ -0,0 +1,165 @@
+import React from "react";
+import "./Group107.css";
+
+function Group107(props) {
+  const {
+    overlapGroup1,
+    vector2,
+    vector3,
+    vector4,
+    vector5,
+    vector6,
+    vector7,
+    vector8,
+    vector9,
+    vector10,
+    vector11,
+    vector12,
+    vector13,
+    vector14,
+    vector15,
+    vector16,
+    group,
+    group2,
+    group3,
+    vector17,
+    group4,
+    group5,
+    group6,
+    vector18,
+    text61,
+    spanText,
+    spanText2,
+    spanText3,
+    spanText4,
+    spanText5,
+    rectangle49,
+    group7,
+    overlapGroup12,
+    vector19,
+    vector20,
+    vector21,
+    text65,
+    text66,
+    rectangle48,
+    vector22,
+    vector23,
+    vector24,
+    vector25,
+    vector26,
+    vector27,
+    vector28,
+    vector29,
+    vector30,
+    text63,
+    text64,
+    rectangle50,
+  } = props;
+
+  return (
+    <div className="group-107">
+      <div className="flex-col-1">
+        <div className="frame-28">
+          <div className="building">
+            <div className="overlap-group-2" style={{ backgroundImage: `url(${overlapGroup1})` }}>
+              <div className="flex-row-1">
+                <div className="flex-row">
+                  <div className="flex-col-6">
+                    <img
+                      className="vector-6"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/60b70661766950cfeb87eb29/img/vector-1@2x.png"
+                    />
+                    <img className="vector-1" src={vector2} />
+                    <img className="vector-1" src={vector3} />
+                    <img className="vector-17" src={vector4} />
+                  </div>
+                  <div className="flex-col-4">
+                    <img className="vector-2" src={vector5} />
+                    <img className="vector-3" src={vector6} />
+                    <img className="vector-3" src={vector7} />
+                    <img className="vector-3" src={vector8} />
+                  </div>
+                  <div className="flex-col-5">
+                    <img className="vector-2" src={vector9} />
+                    <img className="vector-3" src={vector10} />
+                    <img className="vector-3" src={vector11} />
+                    <img className="vector-3" src={vector12} />
+                  </div>
+                  <div className="flex-col-3">
+                    <img className="vector-7" src={vector13} />
+                    <img className="vector-4" src={vector14} />
+                    <img className="vector-4" src={vector15} />
+                    <img className="vector-11" src={vector16} />
+                  </div>
+                </div>
+                <div className="flex-col">
+                  <div className="group" style={{ backgroundImage: `url(${group})` }}></div>
+                  <div className="group-1" style={{ backgroundImage: `url(${group2})` }}></div>
+                  <div className="group-1" style={{ backgroundImage: `url(${group3})` }}></div>
+                  <img className="vector-12" src={vector17} />
+                </div>
+              </div>
+              <div className="flex-col-2">
+                <div className="group-3" style={{ backgroundImage: `url(${group4})` }}></div>
+                <div className="group-2" style={{ backgroundImage: `url(${group5})` }}></div>
+                <div className="group-2" style={{ backgroundImage: `url(${group6})` }}></div>
+                <img className="vector-10" src={vector18} />
+              </div>
+            </div>
+          </div>
+          <div className="frame-29-3">
+            <h1 className="text-1 valign-text-middle typographyheadlineh4-extrabold-24">{text61}</h1>
+            <p className="text-2 typographybodycaption-14-semi-bold">
+              <span className="span typographybodycaption-14-semi-bold">{spanText}</span>
+              <span className="span-1 typographybodycaption-14-semi-bold">{spanText2}</span>
+              <span className="span2 typographybodycaption-14-semi-bold">{spanText3}</span>
+              <span className="span-1 typographybodycaption-14-semi-bold">{spanText4}</span>
+              <span className="span typographybodycaption-14-semi-bold">{spanText5}</span>
+            </p>
+          </div>
+        </div>
+        <img className="rectangle-" src={rectangle49} />
+        <div className="frame-30">
+          <div className="plan">
+            <div className="group-4" style={{ backgroundImage: `url(${group7})` }}>
+              <div className="overlap-group-1" style={{ backgroundImage: `url(${overlapGroup12})` }}>
+                <img className="vector-13" src={vector19} />
+              </div>
+              <img className="vector-16" src={vector20} />
+              <img className="vector-20" src={vector21} />
+            </div>
+          </div>
+          <div className="frame-29-2">
+            <div className="text-5 valign-text-middle typographyheadlineh4-extrabold-24">{text65}</div>
+            <p className="text-6 typographybodycaption-14-semi-bold">{text66}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-col-1">
+        <img className="rectangle-" src={rectangle48} />
+        <div className="frame-29">
+          <div className="blueprint">
+            <div className="overlap-group">
+              <img className="vector-5" src={vector22} />
+              <img className="vector-21" src={vector23} />
+              <img className="vector-14" src={vector24} />
+              <img className="vector-8" src={vector25} />
+              <img className="vector-18" src={vector26} />
+              <img className="vector-9" src={vector27} />
+              <img className="vector-15" src={vector28} />
+              <img className="vector-19" src={vector29} />
+              <img className="vector" src={vector30} />
+            </div>
+          </div>
+          <div className="frame-29-1">
+            <div className="text-3 valign-text-middle typographyheadlineh4-extrabold-24">{text63}</div>
+            <p className="text-4 typographybodycaption-14-semi-bold">{text64}</p>
+          </div>
+        </div>
+        <img className="rectangle-" src={rectangle50} />
+      </div>
+    </div>
+  );
+}
+
+export default Group107;
